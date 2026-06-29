cc27411bded4e9cdcf5d4441489f8ed2309afdda
Hafiz Shaik <hafizullas58@gmail.com>
2026-06-29 13:20:38 +0530

    Fixing the  API

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://veloce-sharie.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://veloce-sharie.onrender.com/api/cars";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
